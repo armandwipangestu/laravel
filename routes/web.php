2feac57ab52467a6878a34658c8f5de6cf9ee731
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-11-12 18:07:39 +0700

    routes: menambahkan data blog_posts ke view posts

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,21 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "author" => "Arman Dwi Pangestu",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, amet autem! Deleniti, quibusdam molestiae labore libero aperiam dicta impedit fugiat mollitia eligendi, laudantium, laboriosam blanditiis dignissimos suscipit! Itaque iste magnam illo vero voluptates ut tempora maxime tempore! Possimus natus, veritatis laboriosam, quam neque ex ad ratione iure temporibus officiis repellat et laborum labore in recusandae! Repudiandae cumque dicta atque ab, omnis deleniti itaque, in rerum amet minus, libero quod enim necessitatibus officiis beatae nihil quasi eligendi! Maiores porro nobis voluptas?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "author" => "Sandhika Galih",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates ea dolores quidem dignissimos reiciendis rerum quae fugiat officia explicabo nesciunt quasi itaque error minima fugit quos, omnis possimus doloremque consequuntur, nisi ducimus ipsa placeat repellat molestias. Eveniet, deleniti maiores dolor quia accusantium amet maxime atque quam perspiciatis aspernatur alias incidunt quos adipisci quo mollitia ipsa voluptatibus numquam architecto laborum voluptatem. Debitis ratione praesentium vitae doloribus, ab recusandae minima asperiores ullam aspernatur beatae! Blanditiis, enim sunt beatae molestias dolor odio impedit! Atque inventore distinctio consequatur accusamus, velit numquam sapiente, laborum cupiditate, cum quidem possimus aspernatur delectus laboriosam est optio commodi unde?"
+        ],
+    ];
+
     return view('posts', [
-        "title" => "Posts"
+        "title" => "Posts",
+        "posts" => $blog_posts
     ]);
 });
